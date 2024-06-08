b0f443aa27793ad017b01cfae4d0cc3e1f02958a
trynaeat <jamesparks10@gmail.com>
2024-06-08 17:02:17 -0400

    Added fns to split each color island into rectangles
    Also includes debug output showing generated rects

--- util/frames_2_code.py
+++ util/frames_2_code.py
@@ -4,6 +4,20 @@ from pathlib import Path
 import cv2
 import numpy as np
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Rect:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    def __str__(self):
+        return f'({self.x},{self.y},{self.width},{self.height})'
+
 def toLuaCode(dict):
     str = ''
     for rgb in dict:
@@ -26,10 +40,9 @@ def colorMask (img):
     masks = dict()
     (w, h, _) = img.shape
     colors = np.unique(img.reshape(-1, 3), axis=0)
-    print(colors)
     for c in colors:
         (r, g, b) = c
-        if np.all(c==[255,255,255]):
+        if np.all(c==[0,0,0]):
             continue
         print(f'Color: {c}')
         mask = np.zeros((h, w), np.uint8)
@@ -46,6 +59,64 @@ def getIslands(img):
     islands = [labels == i for i in range(1, n)]
     return islands
 
+# Check if any of the rects already include this point
+def rectsContain(rects, point):
+    for r in rects:
+        if point.x >= r.x and point.x < r.x + r.width and point.y >= r.y and point.y < r.y + r.height:
+            return True
+    return False
+
+def getRect(rects, img, startPoint):
+    (w, h) = img.shape
+    endX = startPoint.x
+    # Get contiguous white pixels in a row
+    for x in range(startPoint.x, w):
+        isWhite = img[startPoint.y, x]
+        if isWhite and not rectsContain(rects, Point(x, startPoint.y)):
+            endX = x
+        else:
+            break
+    endY = startPoint.y
+    # Then look for any rows of at least same length below
+    for y in range(startPoint.y, h):
+        goodRow = True
+        row = img[y]
+        for j in range(startPoint.x, endX):
+            isWhite = row[j]
+            if not isWhite or rectsContain(rects, Point(j, y)):
+                goodRow = False
+        if goodRow:
+            endY = y
+    return Rect(startPoint.x, startPoint.y, endX - startPoint.x + 1, endY - startPoint.y + 1)
+
+# Split a black/white island into rectangles
+# (x, y, width, height)
+def getRects(img):
+    rects = []
+    (w, h) = img.shape
+    for y in range(0,h):
+        for x in range(0, w):
+            point = Point(x, y)
+            isWhite = img[point.y][point.x]
+            if isWhite and not rectsContain(rects, point):
+                # Start constructing a new rect
+                rects.append(getRect(rects, img, point))
+    return rects
+
+# Test drawing out generated rects in 2 tones
+def debugDrawRects(rects, width, height):
+    i = 0
+    colorWheel = [[255, 0, 0], [0, 255, 0], [0, 0, 255], [128, 128, 128]]
+    img = np.zeros([height, width, 3], dtype=np.uint8)
+    for c in rects.keys():
+        rArr = rects[c]
+        for r in rArr:
+            tone = colorWheel[i % len(colorWheel)]
+            for x in range(r.x, r.x + r.width):
+                for y in range(r.y, r.y + r.height):
+                    img[y][x] = c
+            i = i + 1
+    iio.imwrite(f'DEBUG_rects.png', img)
 
 @click.command()
 @click.argument('dir')
@@ -55,13 +126,15 @@ def getIslands(img):
 @click.option('--out', type=str, default="out", show_default=True)
 def frames2Code(dir, gamma_correction, test_image, filesize, out):
     images = list()
+    colorIslands = dict()
+    rects = dict()
     codeOut = ''
+    # Read all files and gamma correct
     for file in sorted(Path(dir).iterdir()):
         click.echo(file.name)
         if not file.is_file():
             continue
         img = iio.imread(file)
-        masks = colorMask(img)
         invGamma = gamma_correction
         table = np.array([((i / 255.0) ** invGamma) * 255
             for i in np.arange(0, 256)]).astype("uint8")
@@ -69,6 +142,20 @@ def frames2Code(dir, gamma_correction, test_image, filesize, out):
         images.append(img)
     for i, img in enumerate(images):
         imgCode = dict()
+        # split into color islands
+        masks = colorMask(img)
+        for k in masks.keys():
+            islands = getIslands(masks[k])
+            colorIslands[k] = islands
+        # Then get rects for each island
+        for k in colorIslands.keys():
+            islands = colorIslands[k]
+            for isl in islands:
+                r = getRects(isl)
+                if not k in rects:
+                    rects[k] = []
+                rects[k] = rects[k] + r
+        debugDrawRects(rects, 64, 64)
         for y, row in enumerate(img):
             for x, val in enumerate(row):
                 rgb = hex(encodeRGB16(val))[2:]
