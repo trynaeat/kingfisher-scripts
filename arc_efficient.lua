fa71a22f358dafc8068f85b58b4f618574700409
trynaeat <jamesparks10@gmail.com>
2024-06-15 16:40:59 -0400

    Fixed algorithm so it can draw when startAngle > endAngle

--- arc_efficient.lua
+++ arc_efficient.lua
@@ -10,61 +10,86 @@ local floor = math.floor
 local ceil = math.ceil
 local min = math.min
 local max = math.max
+local unpack = table.unpack
 local setColor = screen.setColor
 local drawRectF = screen.drawRectF
 
 local startAngle = 0
 local endAngle = 2 * pi
 
-local checkRange = function(x, y, xrange, yrange)
-	return x >= xrange.min and x <= xrange.max and y >= yrange.min and y <= yrange.max
+-- Can check if point lies in one of multiple ranges
+local checkRange = function(x, y, ranges)
+	for k,range in ipairs(ranges) do
+		if x >= range.min and x <= range.max then
+			return true
+		end
+	end
+	return false
 end
 
-local drawIfInRange = function(x, y, xrange, yrange)
-	if checkRange(x, y, xrange, yrange) then
-		drawRectF(x, y, 1, 1)	
+local drawIfInRange = function(x, y, ranges)
+	if checkRange(x, y, ranges) then
+		drawRectF(x, y, 1, 1)
 	end
 end
 
 local drawCirclePart = function(xc, yc, r, x, y, startPix, endPix)
+	local arcLength = endPix.a - startPix.a
+	if arcLength < 0 then
+		arcLength = 2 * pi + arcLength	
+	end
+	local startSweep = startPix.a - arcLength
+	
 	local xrange
-	if endPix.a <= pi then
+	if startPix.a > endPix.a and endPix.a <= pi then
+		xrange = { min = endPix.x, max = xc + r }
+	elseif startPix.a > endPix.a then
+		xrange = { min = xc - r, max = xc + r }
+	elseif endPix.a <= pi then
 		xrange = { min = endPix.x, max = startPix.x }
 	else
 		xrange = { min = xc - r, max = startPix.x }
 	end
 	local yrange = { min = yc - r, max = yc + r }
 	-- Quadrant 1
-	if startPix.a <= pi / 2 then
-		drawIfInRange(xc + x, yc - y, xrange, yrange)
-		drawIfInRange(xc + y, yc - x, xrange, yrange)
+	if startSweep <= pi then
+		drawIfInRange(xc + x, yc - y, { xrange })
+		drawIfInRange(xc + y, yc - x, { xrange })
 	end
 	-- Quadrant 2
-	if startPix.a <= pi then
-		drawIfInRange(xc - x, yc - y, xrange, yrange)
-		drawIfInRange(xc - y, yc - x, xrange, yrange)
+	if startSweep <= pi then
+		drawIfInRange(xc - x, yc - y, { xrange })
+		drawIfInRange(xc - y, yc - x, { xrange })
 	end
 	-- Restart range for bottom half
-	if startPix.a >= pi and endPix.a > pi then
-		xrange = { min = startPix.x, max = endPix.x }
+	local xranges = {}
+	if startPix.a > endPix.a and startPix.a >= pi and endPix.a > pi then
+		xranges = { { min = xc - r, max = endPix.x }, { min = startPix.x, max = xc + r } }
+	elseif startPix.a > endPix.a and startPix.a >= pi and endPix.a < pi then
+		xranges = { { min = startPix.x, max = xc + r } }
+	elseif startPix.a >= pi and endPix.a > pi then
+		xranges = { { min = startPix.x, max = endPix.x } }
 	elseif startPix.a < pi and endPix.a >= pi then
-		xrange = { min = xc - r, max = endPix.x }
+		xranges = { { min = xc - r, max = endPix.x } }
 	else
-		xrange = { min = xc - r, max = xc - r - 1 }	
+		xranges = { { min = xc - r, max = xc - r - 1 } }
 	end
 	-- Quadrant 3
-	if startPix.a <= 3 * pi / 2 then
-		drawIfInRange(xc - x, yc + y, xrange, yrange)
-		drawIfInRange(xc - y, yc + x, xrange, yrange)
+	if startSweep <= 3 * pi / 2 then
+		drawIfInRange(xc - x, yc + y, xranges)
+		drawIfInRange(xc - y, yc + x, xranges)
 	end
 	-- Quadrant 4
-	if startPix.a <= 2 * pi then
-		drawIfInRange(xc + x, yc + y, xrange, yrange)
-		drawIfInRange(xc + y, yc + x, xrange, yrange)
+	if startSweep <= 2 * pi then
+		drawIfInRange(xc + x, yc + y, xranges)
+		drawIfInRange(xc + y, yc + x, xranges)
 	end
 end
 
 local drawArc = function (xc, yc, radius, startAngle, endAngle)
+	if startAngle < 0 then
+		startAngle = 2 * pi + startAngle	
+	end
 	local startPix = { x = xc + floor(radius * cos(startAngle)), y = yc + floor(radius * sin(startAngle)), a = startAngle }
 	local endPix = { x = xc + ceil(radius * cos(endAngle)), y = yc - floor(radius * sin(endAngle)), a = endAngle }
 	local x = 0
